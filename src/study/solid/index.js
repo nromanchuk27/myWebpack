b8b2a41174e4c083b6d7f31a14eca99d3002d4d0
Nazar <romanenko1927@gmail.com>
2020-08-26 14:12:03 +0300

    add react

---
+++ src/study/solid/index.js
@@ -0,0 +1,24 @@
+// Single Responsibility Principle (один клас відповідає за одну функцію)
+
+class News {
+  constructor(title, text) {
+    this.title = title;
+    this.text = text;
+    this.modified = false;
+  }
+  update(text) {
+    this.text = text;
+    this.modified = true;
+  }
+  toHTML() {
+    return `
+          <div class="news">
+              <h1>${this.title}</h1>  
+              <h2>${this.text}</h2>  
+          </div>
+        `;
+  }
+}
+
+const news = new News("Dynamo", "Won ukrainian Super cup");
+console.log(news.toHTML());
